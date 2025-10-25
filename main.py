ee3b07f9be3fb762a7cc3340cdfdb4a22a47090d
IKHTIAR <ikhti407@gmail.com>
2025-10-26 05:16:41 +0600

    CORS updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ikh-tiar.github.io/youtube-chatbot-frontend/"],  # or ["http://127.0.0.1:5500"] if you open HTML locally
+    allow_origins=["https://ikh-tiar.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
