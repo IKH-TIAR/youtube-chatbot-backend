1ce43fa07f076f480a44f835fbe9453e2dad8ced
IKHTIAR <ikhti407@gmail.com>
2025-10-26 17:03:27 +0600

    CORS Testing

--- main.py
+++ main.py
@@ -9,14 +9,10 @@ from core.text_splitter import split_text
 
 app = FastAPI()
 
-allow_origins=[
-    "https://ikh-tiar.github.io",
-    "https://ikh-tiar.github.io/" # Add with a trailing slash
-],
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
